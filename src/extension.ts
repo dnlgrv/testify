f08728dfd56f4af82e59f9fa68a9c3999461e11b
Daniel Grieve <dnlgrv@hey.com>
2023-05-29 08:25:42 +0100

    Run arbitrary commands in chosen terminal

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,70 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { exec } from 'child_process';
+
+type Terminal = 'integrated' | 'terminal' | 'iterm2';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "testify" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('testify.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from testify!');
+	let execute = vscode.commands.registerCommand("testify.execute", () => {
+		vscode.window.showInputBox({
+			placeHolder: "bundle exec rspec spec",
+			prompt: "Command you would like to execute"
+		}).then(cmd => {
+			if (cmd) {
+				let terminalTarget = vscode.workspace.getConfiguration("testify").get<Terminal>("terminalTarget") || "integrated";
+				run(cmd, terminalTarget);
+			}
+		});
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(execute);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function run(cmd: string, target: Terminal) {
+	switch (target) {
+		case "integrated":
+			runInIntegratedTerminal(cmd);
+			break;
+		case "iterm2":
+			runInIterm2(cmd, "iTerm");
+			break;
+		case "terminal":
+			runInTerminal(cmd, "Terminal");
+			break;
+	}
+}
+
+function runInIterm2(cmd: string, appName: string) {
+	const escape = (s: string) => s.replace(/\'/g, "'\"'\"'");
+
+	const externalCmd =
+    `osascript ` +
+    ` -e 'tell app "iTerm2"' ` +
+    ` -e 'tell current session of current window to write text "${escape(cmd)}"' ` +
+    ` -e 'end tell'`;
+
+	exec(externalCmd);
+}
+
+function runInTerminal(cmd: string, appName: string) {
+	const escape = (s: string) => s.replace(/\'/g, "'\"'\"'");
+
+	const externalCmd =
+    `osascript ` +
+    ` -e 'tell app "Terminal"' ` +
+    ` -e 'do script "${escape(cmd)}" in front window' ` +
+    ` -e 'end tell'`;
+
+	exec(externalCmd);
+}
+
+function runInIntegratedTerminal(cmd: string) {
+	const existingTerminal = vscode.window.terminals[0];
+	existingTerminal.sendText(cmd);
+}
